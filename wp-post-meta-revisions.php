ad0bd3fc175b25dc02d54490023a1e0b669b0432
Adam Silverstein <adam@10up.com>
2014-12-01 21:02:49 -0700

    Rename main file, add deploy script

--- wp-post-meta-revisions.php
+++ wp-post-meta-revisions.php
@@ -1,11 +1,12 @@
 <?php
 /*
-Plugin Name: WP-20564
-Plugin URI: https://github.com/adamsilverstein/wp-20564
-Description: Revisions Post Meta
-Version: 0.3
-Author: Adam Silverstein - code developed with others at https://core.trac.wordpress.org/ticket/20564
-License: GPLv2 or later
+ * Plugin Name: wp-post-meta-revisions
+ * Plugin URI: https://github.com/adamsilverstein/wp-post-meta-revisions
+ * Description: Post Meta Revisions
+ * Version: 0.4
+ * Author: Adam Silverstein - code developed with others
+ * at https://core.trac.wordpress.org/ticket/20564
+ * License: GPLv2 or later
 */
 
 class WP_20564 {
@@ -16,11 +17,23 @@ class WP_20564 {
 	public function __construct() {
 
 		// Actions
+		//
+		// When restoring a revision, also restore that revisions's revisioned meta.
 		add_action( 'wp_restore_post_revision', array( $this, '_wp_restore_post_revision_meta'), 10, 2 );
+
+		// When creating an autosave, save any revisioned meta fields.
 		add_action( 'wp_creating_autosave', array( $this, '_wp_autosave_post_revisioned_meta_fields' ) );
+
+		// When creating a revision, also save ans revisioned meta.
 		add_action( '_wp_put_post_revision', array( $this, '_wp_save_revisioned_meta_fields' ) );
+
+
 		//Filters
+		//
+		// Add the reviosioned meta to get_post_metadata for preview meta data
 		add_filter( 'get_post_metadata',     array( $this, '_wp_preview_meta_filter'), 10, 4 );
+
+		// When revisioned post meta has changed, trigger a revision save
 		add_filter( 'wp_save_post_revision_post_has_changed', array( $this, '_wp_check_revisioned_meta_fields_have_changed' ), 10, 3 );
 
 	}
@@ -52,7 +65,7 @@ class WP_20564 {
 	 * Determine which post meta fields should be revisioned.
 	 *
 	 * @access private
-	 * @since 4.1.0
+	 * @since 4.2.0
 	 *
 	 * @return array An array of meta keys to be revisioned.
 	 */
@@ -60,7 +73,7 @@ class WP_20564 {
 		/**
 		 * Filter the list of post meta keys to be revisioned.
 		 *
-		 * @since 4.1.0
+		 * @since 4.2.0
 		 *
 		 * @param array $keys An array of default meta fields to be revisioned.
 		 */
@@ -108,7 +121,6 @@ class WP_20564 {
 	 * Restore the revisioned meta values for a post
 	 */
 	function _wp_restore_post_revision_meta( $post_id, $revision_id ) {
-		//var_dump( get_metadata( 'post', $revision_id, '_wp_post_revision_meta_keys' ) );
 		// Restore revisioned meta fields; first get the keys for this revision
 		$metas_revisioned =  wp_unslash( get_metadata( 'post', $revision_id, '_wp_post_revision_meta_keys' ) );
 		if ( 0 !== sizeof( $metas_revisioned[0] ) ) {
@@ -132,14 +144,14 @@ class WP_20564 {
 	 * Filters revisioned meta keys only.
 	 *
 	 * @access private
-	 * @since 4.1.0
+	 * @since 4.2.0
 	 *
 	 * @param mixed  $value     Meta value to filter.
 	 * @param int    $object_id Object ID.
 	 * @param string $meta_key  Meta key to filter a value for.
 	 * @param bool   $single    Whether to return a single value. Default false.
 	 * @return mixed Original meta value if the meta key isn't revisioned, the object doesn't exist,
-	 *               the post type is a revisionm or the post ID doesn't match the object ID.
+	 *               the post type is a revision or the post ID doesn't match the object ID.
 	 *               Otherwise, the revisioned meta value is returned for the preview.
 	 */
 	function _wp_preview_meta_filter( $value, $object_id, $meta_key, $single ) {
