0e59380916f19774ea181f631cc9f5554e3dfc1a
Adam Silverstein <adam@10up.com>
2016-03-18 16:50:45 -0600

    Bump version

--- wp-post-meta-revisions.php
+++ wp-post-meta-revisions.php
@@ -3,7 +3,7 @@
  * Plugin Name: Post Meta Revisions
  * Plugin URI: https://github.com/adamsilverstein/wp-post-meta-revisions
  * Description: Post Meta Revisions
- * Version: 0.2.0
+ * Version: 0.2.1
  * Author: Adam Silverstein - code developed with others
  * at https://core.trac.wordpress.org/ticket/20564
  * License: GPLv2 or later
